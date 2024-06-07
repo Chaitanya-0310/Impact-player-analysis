2bd0eb1ad2d6d082519314784cca08bd98959111
Tanmay Damle <tanmay.damle.25@gmail.com>
2024-06-07 14:12:02 -0400

    Database Setup

---
+++ config.py
@@ -0,0 +1,37 @@
+
+from sqlalchemy import create_engine
+from configparser import ConfigParser
+
+def load_config(filename='database.ini', section='postgresql'):
+    parser = ConfigParser()
+    parser.read(filename)
+
+    # get section, default to postgresql
+    config = {}
+   
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            config[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))   
+    
+  
+    config_string = "postgresql://{}:{}@{}:{}/{}".format(config['user'],config['password'],config['host'],config['port'],config['database'])
+    return config_string
+
+def connect(config):
+    """ Connect to the PostgreSQL database server """
+    try:
+        # connecting to the PostgreSQL server
+        engine = create_engine(config) 
+        print('Connected to the PostgreSQL server.')
+        return engine
+    except Exception as error:
+        print(error)
+
+
+if __name__ == '__main__':
+    config_string = load_config()
+    engine = connect(config_string)
+    print(engine)
---
+++ load_data.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+import numpy as np
+from config import load_config, connect
+
+def import_and_load():
+    # import csv file 
+    csv_file_path = os.path.join(os.getcwd(),'deliveries.csv')
+    csv_file = pd.read_csv(csv_file_path)
+
+    csv_file = csv_file.fillna('')
+
+    config_string = load_config()
+    engine = connect(config_string)
+
+    csv_file.to_sql('data',engine,index=False,schema='impact_player_analysis',if_exists='replace')
+
+if __name__ == '__main__':
+    import_and_load()
